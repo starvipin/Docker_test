220835ec8852b8eb782fabd26b20a37b13228ff6
Vipin Saini <starvipin36@gmail.com>
2026-04-28 21:53:04 +0530

    Unit test cases updated

---
+++ tests/test_oprations.py
@@ -0,0 +1,11 @@
+import src.math_oprations as add, sub
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(-1,1) == 0
+
+def terst_sun():
+    assert sub(5,2) == 3
+    assert sub(5,3) == 2
+    assert sub(5,4) == 1
+    assert sub(5,5) == 0
