c9e3e025caf5f8d6d98179bdab4d39e19df857d6
Vipin Saini <starvipin36@gmail.com>
2026-04-30 19:58:44 +0530

    fix error

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,4 +3,4 @@ from src.app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b'hello world'
+    assert response.data == b'hello world!'
