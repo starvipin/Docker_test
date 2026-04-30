52c0f1152be4527e44dde4d4071261450d2dd143
Vipin Saini <starvipin36@gmail.com>
2026-04-30 23:04:16 +0530

    fix hi vipin

--- src/app.py
+++ src/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+<<<<<<< HEAD
     return 'hello world'  
+=======
+    return 'hi vipin'  
+>>>>>>> ac2444d (fix hi vipin)
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", port = 5000 )
