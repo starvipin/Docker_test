5a5ccf2e66d402c4e770787dda75281bfebce2f2
Vipin Saini <starvipin36@gmail.com>
2026-04-30 19:41:06 +0530

    add hll error fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,4 +3,4 @@ from src.app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b'Hell'
+    assert response.data == b'hello world!'
